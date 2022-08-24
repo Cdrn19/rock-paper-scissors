d54bb8920491cba5932cc3e409c79f82b496c291
Carlos Revetti <cdrn_19@hotmail.com>
2022-08-24 16:44:19 -0400

    :sparkles: responsive desing player selection and script

--- assets/modules/script.js
+++ assets/modules/script.js
@@ -2,7 +2,13 @@
 const rulesModal = document.getElementById("rules-modal");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn"); 
-
+/* Play Game input */
+const score = document.getElementById("score");
+const main = document.getElementById("main"); 
+const mainButton = document.getElementsByClassName("main__button"); 
+const playAgainBtn = document.getElementById("playAgainBtn");
+const selection = document.getElementById("selection");
+const selectionResult = document.querySelector(".selection__result");
 /* Modal */ 
 openBtn.addEventListener("click", () => {
     rulesModal.style.display = "grid";  
@@ -11,3 +17,34 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     rulesModal.style.display = "none";
 });
+/* playGame */
+
+for( let i = 0  ; i < mainButton.length ; ++i ) {
+    mainButton[i].addEventListener("click",() => {
+        let select = (mainButton[i].value);
+        main.style.display = "none";    
+        selection.style.display = "flex";
+        selectionResult.style.display = "none"; 
+        selectionResult.style.opacity = "0"; 
+        selectionOptionRender(select, "player","YOU PICKED"); 
+    });
+}
+
+playAgainBtn.addEventListener("click", () => {
+    main.style.display = "grid";
+    selection.style.display = "none";
+});
+
+function selectionOptionRender(selectionPlayer, player,who){
+    document.querySelector(".selection__house").innerHTML = 
+    `<h2>THE HOUSE PICKED</h2>
+    <button class="selection__wait">
+    </button>`;
+    document.querySelector(`.selection__${player}`).innerHTML = 
+    `<h2>${who}</h2>
+    <button class="selection__button ${selectionPlayer}">
+        <figure class="selection__button--figure ${selectionPlayer}">
+            <img src="./assets/images/icon-${selectionPlayer}.svg" alt="${selectionPlayer}-icon">
+        </figure>
+    </button>`;
+}
